30ab9be2f83e64353bbc9ee2599088f00942d255
MUHAMMAD-AMMAR-TANWEER <ammartanweer7370@gmail.com>
2023-03-03 02:46:42 -0800

    first phase

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,55 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+
+import { Box, Center, Spacer, Stack } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import NavBar from '../components/Navbar'
+import Disconnected from '../components/Disconnected'
+import { useWallet } from '@solana/wallet-adapter-react'
+import Connected from '../components/Connected'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { connected } = useWallet()
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={'url(/home-background.svg)'}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+          {/* NavBar */}
+          <NavBar />
+          <Spacer />
+          <Spacer />
+          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
+          <Spacer />
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Center>
+            <Box marginBottom={4} color="white">
+              <a
+                href="https://twitter.com/_buildspace"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                built with @_buildspace
+              </a>
+            </Box>
+          </Center>
+        </Stack>
+      </Box>
+    </div>
   )
 }
